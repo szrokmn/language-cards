16dc381864c5dff0084cdc9d1705ad745dcf4ee2
Sezer Ökmen <szrokmn34@gmail.com>
2022-10-18 15:24:34 +0300

    css finished

--- src/components/Language.jsx
+++ src/components/Language.jsx
@@ -5,17 +5,17 @@ const Language = ({name, img, options}) => {
   // console.log(lang);
   const [showImage, setShowImage] = useState(true)
   return (
-    <Container style={{background:"peachpuff"}} className="p-4 rounded-2" onClick={() => setShowDesc(!showDesc)}>
+    <Container style={{background:"peachpuff"}} type="button" className="lang-card p-4 rounded-2 h-100" onClick={() => setShowDesc(!showDesc)}>
 
       {showImage && (
         <Container>
-        <Image src={img} width="70%"></Image>
+        <Image className="lang-logo" src={img} width="70%"></Image>
         <h3 className="display-6">{name}</h3>
       </Container>
       )}    
 
       {!showImage && (
-       <ol>
+       <ol className="h-100 d-flex flex-column justify-content-center">
        {options.map((item) => {
          return 
          <li className="h6 text-start">{item}</li>
