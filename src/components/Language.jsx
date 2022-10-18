5029bd77f53d49bd68eb6132d32df2ddc13a62a6
Sezer Ökmen <szrokmn34@gmail.com>
2022-10-18 14:28:09 +0300

    language showImage true-false

--- src/components/Language.jsx
+++ src/components/Language.jsx
@@ -3,12 +3,26 @@ import Image from "react-bootstrap/Image"
 
 const Language = ({name, img, options}) => {
   // console.log(lang);
+  const [showImage, setShowImage] = useState(true)
   return (
-    <Container style={{background:"peachpuff"}} className="p-4 rounded-2">
-      <Container>
+    <Container style={{background:"peachpuff"}} className="p-4 rounded-2" onClick={() => setShowDesc(!showDesc)}>
+
+      {showImage && (
+        <Container>
         <Image src={img} width="70%"></Image>
         <h3 className="display-6">{name}</h3>
       </Container>
+      )}    
+
+      {!showImage && (
+       <ol>
+       {options.map((item) => {
+         return 
+         <li className="h6 text-start">{item}</li>
+       })}
+     </ol>
+      )}          
+
     </Container>
   )
 }
